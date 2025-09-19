99f6d4d7d18c6e0b939ad233c30a9ddd74bda4b7
Fabio <fabio_almeida89@outlook.com>
2025-09-19 15:34:57 -0300

    fazendo commit

---
+++ exemplos/bicycles.py
@@ -0,0 +1,3 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "Minha primeira bicicleta foi uma: " + bicycles[0].title() + "."
+print(message)
--- exercicios/exerc_2.1-nome/nomes.py
+++ exercicios/exerc_2.1-nome/nomes.py
@@ -1,5 +1,2 @@
 names = ["Fabio", "Pedro", "Poli", "Amanda"]
-print(names[0].title())
-print(names[1].title())
-print(names[2].title())
-print(names[3].title())
+message = ""
