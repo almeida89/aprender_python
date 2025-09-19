3e560ab9c9b66d47d2bb995ed6f6a400a4de5819
Fabio <fabio_almeida89@outlook.com>
2025-09-19 15:42:55 -0300

    Adiciona lista de transportes preferidos e exibe frases

---
+++ exercicios/exer_2.3-my_list/my_list.py
@@ -0,0 +1,10 @@
+list = ["lamborgini", "mercedes", "skate", "bicicleta"]
+message1 = "Eu adoraria ter uma, "
+message2 = "Meu sonho andar de, "
+message3 = "Um dia volto a andar de, "
+message4 = "Eu amo andar de, "
+
+print(message1 + list[0].title())
+print(message2 + list[1].title())
+print(message3 + list[2].title())
+print(message4 + list[3].title())
