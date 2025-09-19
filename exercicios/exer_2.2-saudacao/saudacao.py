3f5a5009af82017fc4b7dfba3532839910d79ee9
Fabio <fabio_almeida89@outlook.com>
2025-09-19 15:32:16 -0300

    feat: Personaliza mensagens para cada nome em uma lista
    Modifica o exercício anterior para iterar sobre uma lista de nomes e exibir uma mensagem personalizada para cada pessoa...

---
+++ exercicios/exer_2.2-saudacao/saudacao.py
@@ -0,0 +1,6 @@
+names = ["Fabio", "Pedro", "Poli", "Amanda"]
+messageF = "Olá como vai, "
+print(messageF + names[0])
+print(messageF + names[1])
+print(messageF + names[2])
+print(messageF + names[3])
