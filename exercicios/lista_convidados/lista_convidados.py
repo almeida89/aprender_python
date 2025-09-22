e5a10aedb5f25229030a440dcc0c5551d1580776
Fabio <fabio_almeida89@outlook.com>
2025-09-22 16:46:53 -0300

    Exercico de conteúdo

---
+++ exercicios/lista_convidados/lista_convidados.py
@@ -0,0 +1,12 @@
+'''Se pudesse convidar alguém, vivo ou morto, para o jantar, quem você convidaria? 
+Crie uma lista que inclua pelo menos três pessoas que você gostaria de convidar para jantar. 
+Em seguida, utilize sua lista para exibir uma mensagem para cada pessoa, convidando-a para jantar'''
+
+
+convidados = ["Jimi Hendrix ", "Ozzy Osbourne", "Jim Morrison", "Kurt Cobain"]
+convite1 = ", você está convidado para o nosso jantar do Rock!"
+
+print(convidados[0].title() + convite1)
+print(convidados[1].title() + convite1)
+print(convidados[2].title() + convite1)
+print(convidados[3].title() + convite1)
