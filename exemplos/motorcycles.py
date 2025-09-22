c2c4aff8f0f1dfab57773e2697bbf054f9b2ec16
Fabio <fabio_almeida89@outlook.com>
2025-09-22 16:12:15 -0300

    Usando o método remove()

--- exemplos/motorcycles.py
+++ exemplos/motorcycles.py
@@ -1,3 +1,4 @@
-motos = ['honda', 'yamaha', 'suzuki']
-first_owned = motos.pop(0)
-print("A primeira moto que eu comprei, foi a " + first_owned.title() + ".")
+motos = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motos)
+motos.remove('ducati')
+print(motos)
