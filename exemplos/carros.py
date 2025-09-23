62ee1f15574cfb46292ed528e1706de264275ef1
Fabio <fabio_almeida89@outlook.com>
2025-09-23 16:46:54 -0300

    Utilizado o método sorted()

--- exemplos/carros.py
+++ exemplos/carros.py
@@ -1,3 +1,9 @@
 cars = ["bmw", "audi", "toyota", "subaru"]
-cars.sort(reverse=True)
+print("Essa é a lista original: ")
+print(cars)
+
+print("\nAqui está a lista classificada")
+print(sorted(cars))
+
+print("\nAqui está a lista original novamente:")
 print(cars)
