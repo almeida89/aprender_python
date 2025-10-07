b42e29c97bb32f8ce8af4e841ac12fe3f26fa899
Fabio <fabio_almeida89@outlook.com>
2025-10-07 15:38:47 -0300

    Utilizado o len() nesse exercicio

---
+++ exercicios/3.9 - Convidados para o jantar/Convidados_para_o_jantar.py
@@ -0,0 +1,4 @@
+'''Trabalhando com seu programa de convidados, 
+use len() para exibir uma mensagem informando o número de pessoas que você está convidando para o jantar'''
+convidados = ["Jimi Hendrix ", "Ozzy Osbourne", "Jim Morrison", "Kurt Cobain"]
+print("Estamos convidando para o jantar " + str(len(convidados)) + " para o jantar!")
