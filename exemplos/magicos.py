47b20c72bf6950bb3e649986f759aeb3536f01e2
Fabio <fabio_almeida89@outlook.com>
2025-10-08 20:18:52 -0300

    Introduzindo o for dentro de listas

---
+++ exemplos/magicos.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians: 
+    print(magician.title() + ", esse é um ótimo truque") 
+    print("Mal posso esperar para ver seu próximo truque, " + magician.title() + ".\n")
+print("Obrigado a todos. Foi um show de mágica incrível!")
